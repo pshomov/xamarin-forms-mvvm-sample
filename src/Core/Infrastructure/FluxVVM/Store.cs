c21b1e62769bda9d8608b6722812569ad2156901
Petar Shomov <pshomov@gmail.com>
2015-09-14 10:09:51 +0000

    I got it, now porting Redux to .NET

---
+++ src/Core/Infrastructure/FluxVVM/Store.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace XamarinFormsTester.Infrastructure.FluxVVM
+{
+    public struct Action<A> {
+        public A type;
+    }
+    public delegate State Reducer<State, Actions>(State state, Action<Actions> action);
+    public class Store<State, Actions>
+    {
+        public void dispatch (Action<Actions> action)
+        {
+            this._state = rootReducer.Invoke (this._state, action);
+        }
+
+        public State getState ()
+        {
+            return _state;
+        }
+
+        State _state;
+
+        Reducer<State, Actions> rootReducer;
+
+        public Store (Reducer<State, Actions> rootReducer, State initialState)
+        {
+            this.rootReducer = rootReducer;
+            this._state = initialState;
+        }
+    }
+}
+
---
+++ src/UnitTests/FluxVVM/StoreTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using XamarinFormsTester.Infrastructure.FluxVVM;
+using System.Collections.Generic;
+using System;
+
+namespace XamarinFormsTester.UnitTests.FluxVVM
+{
+    [TestFixture]
+    public class StoreTests
+    {
+        enum Actions {
+            ADD_ITEM,
+            REMOVE_ITEM
+        }
+
+        [Test]
+        public void should_register_root_reducer(){
+            
+            Reducer<List<string>, Actions> reducer = (List<string>state, XamarinFormsTester.Infrastructure.FluxVVM.Action<Actions> action) =>  {                
+                var newState = new List<string> (state);
+                newState.Add("Read the Redux docs");
+                return newState;
+            };
+            var store = new Store<List<String>, Actions> (reducer, new List<string>{ "Use FluxVVM" });
+            store.dispatch (new XamarinFormsTester.Infrastructure.FluxVVM.Action<Actions>{type = Actions.ADD_ITEM});
+
+            CollectionAssert.AreEqual(store.getState(), new List<string>{"Use FluxVVM", "Read the Redux docs"});
+        }
+    }
+}
+
