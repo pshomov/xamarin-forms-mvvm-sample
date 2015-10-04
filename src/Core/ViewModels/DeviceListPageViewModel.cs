28b4d980590629213bb63410571197ef74b621bc
Petar Shomov <pshomov@gmail.com>
2015-10-04 10:02:22 +0000

    preparing for testing the viewmodel updating

--- src/Core/ViewModels/DeviceListPageViewModel.cs
+++ src/Core/ViewModels/DeviceListPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using XamarinFormsTester.Infrastructure.ReduxVVM;
+using System.Linq;
 
 namespace XamarinFormsTester.ViewModels
 {
@@ -7,6 +9,14 @@ namespace XamarinFormsTester.ViewModels
     {
         public ObservableCollection<DeviceSummary> Devices { get; set;}
         public Boolean Pulling { get; set;}
+
+        public DeviceListPageViewModel (Store<AppState> store)
+        {
+            store.Subscribe ((s) => {
+                Pulling = s.DevicePage.inProgress;
+                Devices = new ObservableCollection<DeviceSummary>(s.DevicePage.Devices.Select(d => new DeviceSummary{Name = d.Name, Location = d.Location}));
+            });
+        }
     }
 }
 
